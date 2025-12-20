5b59bdfec90ab6653368f3a501bea4d6b08ec4f5
jitendra kumar singhal <jiten.cs2010@gmail.com>
2025-12-20 17:08:58 +0530

    batch size - alternate solution of 1 + N problem

--- src/main/java/org/example/productcatelogservice1/models/Category.java
+++ src/main/java/org/example/productcatelogservice1/models/Category.java
@@ -5,6 +5,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
@@ -17,6 +18,7 @@ public class Category extends BaseModel {
     private String name;
     private String description;
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "category")
-    @Fetch(FetchMode.SUBSELECT)
+    @Fetch(FetchMode.SELECT)
+    @BatchSize(size=4)
     private List<Product> products;
 }
